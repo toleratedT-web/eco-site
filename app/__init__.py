efbcb962c722526886c45796d5ad9b9009da7508
Oliver <oliversanderson26@gmail.com>
2025-12-18 14:33:57 +0000

    matthew im cooler than u

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
+from flask_mail import Mail
 
 # Initialize extensions globally
 db = SQLAlchemy()
